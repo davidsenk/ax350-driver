26b0098caebe1c1fb7f933c1272cb7575c5c6abb
David Senk <david@senk.me>
2017-03-02 17:45:34 -0500

    Added binary encoder

--- ax350-driver.ino
+++ ax350-driver.ino
@@ -14,10 +14,29 @@ void enableMux(){
   digitalWrite(enablerPins[1], LOW);
 }
 
+void flashMux(){
+  //make sure you start off disabled
+  disableMux();
+
+  //connect ends together for 125ms, simulating a keypress.
+  enableMux();
+  delay(125);
+  disableMux();
+}
+
+void resetMuxes(){
+  for(int i = 0; i < 3; i++){
+    digitalWrite(mux1[i], LOW);
+    digitalWrite(mux2[i], LOW);
+  }
+  disableMux();
+}
+
 //connects pins via the muxes
 void connectPins(int pin1, int pin2, bool leaveDisabled = false){
   //disable muxes as connections are being changed
   disableMux();
+  resetMuxes();
 
   //if pin connections dont make sense, dont try and connect them
   if(pin1 > 7 || pin2 > 7){
@@ -26,6 +45,41 @@ void connectPins(int pin1, int pin2, bool leaveDisabled = false){
   if(pin1 < 0 || pin2 < 0){
     return;
   }
+
+  for(int i = 0; i < 3; i++){
+    int lpin1 = pin1 & (1 << i);
+    int lpin2 = pin2 & (1 << i);
+    //Serial.print(i);
+    //Serial.print("-");
+    //Serial.println(lpin1);
+    Serial.print("MUX1 # ");
+    Serial.print(i);
+    if(lpin1){
+      Serial.print(" HIGH ");
+      digitalWrite(mux1[i], HIGH);
+    } else {
+      Serial.print(" LOW  ");
+      digitalWrite(mux1[i], LOW);
+    }
+    Serial.print(mux1[i]);
+    Serial.print(" MUX2 # ");
+    Serial.print(i);
+    if(lpin2){
+      Serial.print(" HIGH ");
+      digitalWrite(mux2[i], HIGH);
+    } else {
+      Serial.print(" LOW  ");
+      digitalWrite(mux2[i], LOW);
+    }
+    Serial.println(mux2[i]);
+  }
+  
+
+  //leave pins muxes disabled if desired
+  if(leaveDisabled){
+    return;
+  }
+  enableMux();
 }
 
 void setup() {
@@ -38,15 +92,31 @@ void setup() {
 }
 
 
+void testConnect(){
+    for(int i = 0; i < 8; i++){
+    for(int i2 = 0; i2 < 8; i2++){
+      Serial.print("Attempting to connect ");
+      Serial.print(i);
+      Serial.print(" - ");
+      Serial.println(i2);
+      connectPins(i, i2, true);
+      delay(500);
+    }
+  }
+}
+
+void blinkenlights(){
+  //test mux control
+  enableMux();
+  TXLED1;
+  delay(5000);
+  disableMux();
+  TXLED0;
+  delay(5000);
+}
 
 
 
 void loop() {
-  //test mux control
-  enableMux();
-  TXLED1;
-  delay(3000);
-  disableMux();
-  TXLED0;
-  delay(3000);
+  testConnect();
 }
